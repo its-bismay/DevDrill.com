c2fd31ff909ae42ce11a1418adbd9a28a6a7485c
its-bismay <2023ugee070@nitjsr.ac.in>
2025-12-13 15:40:48 +0530

    01: database and tool setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import { ENV } from "./env.js";
+import mongoose from "mongoose"
+
+export const connectDB =async () => {
+    try {
+        const conn = await mongoose.connect(ENV.DB_URL);
+        console.log("connected to the database", conn.connection.host)
+    } catch (error) {
+        console.log("error connecting to the database", error)
+        process.exit(1)
+    }
+}
