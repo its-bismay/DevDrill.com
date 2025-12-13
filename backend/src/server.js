27cc44bef07b7ce32fd99a8702ebbfcefc08986c
Bibhabasu Bismay Kumar Naik <155875271+its-bismay@users.noreply.github.com>
2025-12-13 14:29:48 +0530

    Disable production static file serving
    Commented out production environment code for static files.

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,14 +14,15 @@ app.get("/books", (req, res) => {
     res.status(200).json({message: "book endpoint is up and running"})
 })
 
-if(ENV.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if(ENV.NODE_ENV == "production"){
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/{*any}", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
-}
+//     app.get("/{*any}", (req,res) => {
+//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+//     })
+// }
 
 app.listen(ENV.PORT, () => {
     console.log(`server started successfully on port ${ENV.PORT}`)
+
 })
